5c9f2bfbb16e0f8cc5500cfdba98ab2649d659f7
Your Name <rajatpandey542@gmail.com>
2026-06-25 15:46:39 +0530

    Test connection of Database with server using pool

--- Server/index.js
+++ Server/index.js
@@ -2,31 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const loginRoutes=require('./routes/auth.js')
-const user=require('./routes/user.js');
-const { Pool } = require("pg");
-require("./db");
+const loginRoutes = require("./routes/auth.js");
+const user = require("./routes/user.js");
+const pool = require("./db.js");
 
+app.get("/health", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      status: "OK",
+      Time: new Date(result.rows[0].now).toLocaleString(),
+    });
+  } catch {
+    console.log("Error");
+    res.status(500).json({
+      status: "Database connection failed",
+    });
+  }
+});
 app.get("/", (req, res) => {
   res.end("Hello world yoo");
 });
-app.use("/users",user)
-app.use("/auth", loginRoutes)
-app.get("/health", async (req, res) => {
-  try{
-    const result = await Pool.query('SELECT NOW');
-    res.json({
-      status:"OK",
-      Time:result.rows[0].now
-    });
-  }
-  catch{
-    console.log("Error")
-    res.status(500).json({
-      status:'Database connection failed',
-    });
-  }
-});
+app.use("/users", user);
+app.use("/auth", loginRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
